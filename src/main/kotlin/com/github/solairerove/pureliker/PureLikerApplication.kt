0f1526a6141924020d918b515ea65482e24bf3dd
solairerove <solairerove@gmail.com>
2021-11-30 02:43:48 +0300

    feature: get correct? server time

--- src/main/kotlin/com/github/solairerove/pureliker/PureLikerApplication.kt
+++ src/main/kotlin/com/github/solairerove/pureliker/PureLikerApplication.kt
@@ -4,6 +4,8 @@ import org.apache.commons.codec.binary.Hex
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.time.Instant
+import java.util.*
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
@@ -41,19 +43,43 @@ calculateAuthorizationHash(type, url, requestBody, userId) {
   }
  */
 
+/*
+next problem is get correct server time
+
+  getServerTime(time) {
+    const clientTime = time * 1000 || Date.now();
+    return (clientTime + Number(this.storage.get('deltaTime'))) / 1000;
+  }
+
+  synchroniseTime(responseBody) {
+    if (_.get(responseBody, 'additionalInfo.serverTime')) {
+      const serverTime = parseInt((responseBody.additionalInfo.serverTime * 1000).toString(), 10);
+      const clientTime = Date.now();
+      this.storage.set('deltaTime', serverTime - clientTime);
+    }
+  }
+ */
+
 /**
  * this.storage.get('sessionToken') cookie?
  * userId есть
  * serverTime проблема
- * hash вообще пиздец честно говоря
+ * hash вообще пиздец честно говоря (больше не пиздец)
  */
 @SpringBootApplication
 class PureLikerApplication : CommandLineRunner {
 
     override fun run(vararg args: String?) {
         println("hui")
-        val sToEncode = "GET+/me++1638226516.123"
-        println(createSignature(sToEncode, "b4255022d6f65a35622b700c96a84dd8"))
+
+        val currTime = (Date.from(Instant.now()).time).toString()
+        val serverTime = "${currTime.take(currTime.length - 3)}.${currTime.takeLast(3)}"
+        println(serverTime)
+
+        println(createSignature("GET+/me++1638229151.213", "6d30252367eaa0630c4ba38481a33a84"))
+
+        val sToEncode = "GET+/me++$serverTime"
+        println("hmac 6019a921ae5703021800365c:$serverTime:${createSignature(sToEncode, "6d30252367eaa0630c4ba38481a33a84")}")
     }
 }
 
@@ -63,17 +89,18 @@ fun main(args: Array<String>) {
 
 // https://www.freeformatter.com/hmac-generator.html#ad-output
 // example of necessary hmac
-// hmac 6019a921ae5703021800365c:1638226516.123:1d5a9b7c6f4b23293aaa51ed2262e87815b2e7f406d4e195c32b610fc8c396e1
+// hmac 6019a921ae5703021800365c:1638229151.213:aa5cd4111be3900e494fef91cb92e896405f5acf4de31aff0f62b9966b0d1103
 // userId - 6019a921ae5703021800365c
 // serverTime - 1638226516.123
 // hash 1d5a9b7c6f4b23293aaa51ed2262e87815b2e7f406d4e195c32b610fc8c396e1
-fun createSignature( data: String, key: String): String {
+fun createSignature(data: String, key: String): String {
     val sha256Hmac = Mac.getInstance("HmacSHA256")
     val secretKey = SecretKeySpec(key.toByteArray(), "HmacSHA256")
     sha256Hmac.init(secretKey)
 
     return Hex.encodeHexString(sha256Hmac.doFinal(data.toByteArray()))
+}
 
-    // For base64
-    // return Base64.getEncoder().encodeToString(sha256Hmac.doFinal(data.toByteArray()))
+fun getServerTime() {
+
 }
