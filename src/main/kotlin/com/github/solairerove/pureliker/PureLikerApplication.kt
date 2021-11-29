990f7a1c9a39acc7cbb37815584bf79d50b1f3be
solairerove <solairerove@gmail.com>
2021-11-30 01:32:46 +0300

    feature: initial start

---
+++ src/main/kotlin/com/github/solairerove/pureliker/PureLikerApplication.kt
@@ -0,0 +1,11 @@
+package com.github.solairerove.pureliker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PureLikerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PureLikerApplication>(*args)
+}
