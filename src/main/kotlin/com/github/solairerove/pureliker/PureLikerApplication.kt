9b0d450897a23df1714422d760c56d16d689eae7
solairerove <solairerove@gmail.com>
2021-12-04 01:58:44 +0300

    feature: get tyan page feed due rest template

--- src/main/kotlin/com/github/solairerove/pureliker/PureLikerApplication.kt
+++ src/main/kotlin/com/github/solairerove/pureliker/PureLikerApplication.kt
@@ -8,6 +8,7 @@ import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.StandardOpenOption
@@ -149,25 +150,8 @@ class PureLikerApplication : CommandLineRunner {
         Files.write(myfile.toPath(), ("ele" + "\n").toByteArray(), StandardOpenOption.APPEND)
         Files.write(myfile.toPath(), ("ele2" + "\n").toByteArray(), StandardOpenOption.APPEND)
 
-//        val restTemplate = RestTemplate()
-//        val feedUrl = "/search/feed/?city_id=524901&have_photo=true&is_around_city=true&is_online=true&lang=ru&looking_for=f&ordering=-is_online%2C-created_at&page=1&sexuality=h&start_at=$serverTime"
-//        val apiHost = "https://pure-api.soulplatform.com"
-//
-//        val payloadToHash = "GET+$feedUrl++$serverTime"
-//
-//        val headers = HttpHeaders()
-//        headers.add("x-js-user-agent", "PureFTP/1.8.6 (JS 1.0; OS X 10.15.7 64-bit MacIntel Chrome 95.0.4638.69; en-US) SoulSDK/0.14.6 (JS)")
-//        headers.add("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
-//        headers.add("sec-ch-ua", "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"")
-//        headers.add("accept", "*/*")
-//        headers.add("accept-encoding", "gzip, deflate, br")
-//        val hmacToken = "hmac 6019a921ae5703021800365c:$serverTime:${createSignature(payloadToHash, "c5298114379096c4da2db73fb4f311db")}"
-//        headers.add("authorization", hmacToken)
-//
-//        val req = HttpEntity<Any>(headers)
-//        val response = restTemplate.exchange("$apiHost/search/feed/?city_id=524901&have_photo=true&is_around_city=true&is_online=true&lang=ru&looking_for=f&ordering=-is_online,-created_at&page=1&sexuality=h&start_at=$serverTime", HttpMethod.GET, req, String::class.java)
-
-        getMe()
+//        getMe()
+        getFeedPage()
     }
 }
 
@@ -207,6 +191,77 @@ fun getMe() {
         HttpMethod.GET,
         req,
         String::class.java
+    )
+
+    response.statusCodeValue
+}
+
+fun getFeedPage() {
+    val serverTime = stringifyServerTime(currTime = getServerTime())
+
+    val restTemplate = RestTemplate()
+    val feedUrl = "/search/feed/"
+    val apiHost = "https://pure-api.soulplatform.com"
+
+    val payloadToHash =
+        "GET+/search/feed/?city_id=524901&have_photo=true&is_around_city=true&is_online=true&lang=ru&looking_for=f&ordering=-is_online,-created_at&page=1&sexuality=h&start_at=$serverTime++$serverTime"
+
+    val headers = HttpHeaders()
+    headers.add(
+        "x-js-user-agent",
+        "PureFTP/1.8.6 (JS 1.0; OS X 10.15.7 64-bit MacIntel Chrome 95.0.4638.69; en-US) SoulSDK/0.14.6 (JS)"
+    )
+    headers.add(
+        "user-agent",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    )
+    headers.add("sec-ch-ua", "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"")
+    headers.add("accept", "*/*")
+    headers.add("accept-encoding", "gzip, deflate, br")
+
+    val hmacToken = "hmac 6019a921ae5703021800365c:$serverTime:${
+        createSignature(
+            payloadToHash,
+            "c5298114379096c4da2db73fb4f311db"
+        )
+    }"
+
+    headers.add("authorization", hmacToken)
+
+    val req = HttpEntity<Any>(headers)
+
+    val urlTemplate = UriComponentsBuilder.fromHttpUrl("$apiHost$feedUrl")
+        .queryParam("city_id", "{city_id}")
+        .queryParam("have_photo", "{have_photo}")
+        .queryParam("is_around_city", "{is_around_city}")
+        .queryParam("is_online", "{is_online}")
+        .queryParam("lang", "{lang}")
+        .queryParam("looking_for", "{looking_for}")
+        .queryParam("ordering", "{ordering}")
+        .queryParam("page", "{page}")
+        .queryParam("sexuality", "{sexuality}")
+        .queryParam("start_at", "{start_at}")
+        .encode()
+        .toUriString()
+
+    val params: MutableMap<String, String> = HashMap<String, String>()
+    params["city_id"] = "524901"
+    params["have_photo"] = "true"
+    params["is_around_city"] = "true"
+    params["is_online"] = "true"
+    params["lang"] = "ru"
+    params["looking_for"] = "f"
+    params["ordering"] = "-is_online,-created_at"
+    params["page"] = "1"
+    params["sexuality"] = "h"
+    params["start_at"] = serverTime
+
+    val response = restTemplate.exchange(
+        urlTemplate,
+        HttpMethod.GET,
+        req,
+        String::class.java,
+        params
     )
 
     response.statusCodeValue
